000990d2ce12ef4d537e7a285937bc81cdcedc80
github username <github email address>
2019-04-05 15:56:31 -0400

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+  :attr_reader
+
+  def initialize
+  end
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+end
